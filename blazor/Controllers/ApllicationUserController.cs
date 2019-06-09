8527d26a78f37c5b0a9f63a0c4224d3bfc537471
Ahmed Magdi <magdi@Ahmeds-MacBook-Pro.local>
2019-06-10 00:07:54 +0200

    spa added

---
+++ blazor/Controllers/ApllicationUserController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace blazor.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApllicationUserController : ControllerBase
+    {
+        
+        public ApllicationUserController()
+        {
+            
+        }
+
+    }
+}
